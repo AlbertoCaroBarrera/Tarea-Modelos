19ee3d9f55a3ff95266daf6eea1020c6c6ac5e2e
alberto <albertocarobarrera@gmail.com>
2023-10-13 21:23:30 +0200

    ejercicio terminado

--- escenario/models.py
+++ escenario/models.py
@@ -14,17 +14,12 @@ class Tarea(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     prioridad = models.IntegerField()
-    estado = [('Pendiente','Progreso','Completada')]
+    estado = [('PE','Pendiente'),('PR','Progreso'),('Co','Completada')]
     completada = models.BooleanField()
     fecha_creacion = models.DateTimeField(default = timezone.now)
     hora_vencimiento = models.TimeField()
-    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    usuarios_asignados = models.ManyToManyField(Usuario, through='Asignacion_tarea')
-    
-
-class Asignacion_tarea(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    tarea = models.ForeignKey(Tarea,on_delete=models.CASCADE)
+    creador_tarea = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="creador_tarea")
+    usuarios_asignados = models.ManyToManyField(Usuario, through='Asignacion_tarea', related_name="usuarios_asignados")
 
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=200)
@@ -32,20 +27,22 @@ class Proyecto(models.Model):
     duracion = models.FloatField()
     fecha_inicio = models.DateTimeField(default = timezone.now)
     fecha_fin = models.DateTimeField()
-    usuario = models.ManyToManyRel(Usuario)
-    creador = models.OneToOneField(Usuario,on_delete=models.CASCADE)
-    
+    usuario = models.ManyToManyField(Usuario, related_name="usuario") 
+    creador = models.OneToOneField(Usuario,on_delete=models.CASCADE,related_name="creador")
+    tareas = models.ManyToManyField(Tarea)
 
 class Etiqueta(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200,unique=True)
     tarea = models.ManyToManyField(Tarea)
     
 class Asignacion_tarea(models.Model):
+    tarea = models.ForeignKey(Tarea,on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     observaciones = models.TextField()
     fecha_asignacion = models.DateTimeField(default = timezone.now)
     
 class Comentario(models.Model):
     contenido = models.TextField()
     fecha_comentario = models.DateTimeField(default = timezone.now)
-    autor = models.ManyToOneRel(Usuario,on_delete=models.CASCADE)
-    tarea = models.ManyToOneRel(Tarea,on_delete=models.CASCADE)
+    autor = models.ForeignKey   (Usuario,on_delete=models.CASCADE)
+    tarea = models.ForeignKey(Tarea,on_delete=models.CASCADE)
